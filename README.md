a1b5e729f12c219722c1a83de98bab7664d335f4
Karim Bridi <karimbridi@gmail.com>
2023-07-20 21:23:00 -0300

    entrega

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1 align="center">Hi 👋, I'm Karim</h1>
+<h3 align="center">A passionate full stack developer student from Argentina</h3>
+
+<h3 align="left">Languages and Tools learned:</h3>
+<p>- HTML<br>
+        - CSS<br>
+        - Git<br>
+        - SASS<br>
+        - Bootstrap<br>
+        - Javascript</p>
--- js/main.js
+++ js/main.js
@@ -1,2 +1,47 @@
 
 //Insertar codigo
+
+let boton = document.getElementById("start")
+boton.addEventListener("click", startCode)
+function startCode () {
+
+    alert("Prueba mensaje")
+
+}
+
+
+
+/* alert("Bienvenido usuario a esta calculadora de numeros factoriales")
+let factorial1 = Number(prompt("Ingresa el primer número entero"))
+alert("El resultado de "+factorial1+"! es: "+resulFac(factorial1))
+let factorial2 = Number(prompt("Ingresa el segundo número entero"))
+alert("El resultado de "+factorial2+"! es: "+resulFac(factorial2))
+let operacion = prompt("¿Qué operación desea realizar con "+factorial1+"! y "+factorial2+"! [+][-][*]?")
+if (operacion == "+" || operacion == "-" || operacion == "*") {
+    alert("El resultado de "+factorial1+"! "+operacion+" "+factorial2+"! es: "+op(operacion))
+} else {
+    alert("Ingresó una operación inválida")
+}
+
+function resulFac (factorial){
+    if (factorial == 0 || factorial < 0) {
+        return(0)
+    } else {
+        let x = 1
+        for (let i = factorial; i > 0; i--) {
+            x = x * i
+        }
+        return(x)
+    }
+}
+
+function op (operador){
+    switch (operador) {
+        case "+":
+            return resulFac(factorial1) + resulFac(factorial2);
+        case "-":
+            return resulFac(factorial1) - resulFac(factorial2);
+        case "*":
+            return resulFac(factorial1) * resulFac(factorial2);
+    }
+} */
